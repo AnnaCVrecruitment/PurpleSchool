4bac19c1b26a151ee7785c5527493034a65c1008
Anna Kirova <akirova@smile-mobile.ru>
2025-08-22 11:08:14 +0300

    HW - fix words

--- 2-typings/2-typings.ts
+++ 2-typings/2-typings.ts
@@ -48,7 +48,7 @@ function toWords(number: number | string, asOrdinal?: boolean): string {
   return asOrdinal ? makeOrdinal(words) : words;
 }
 
-function generateWords(number: number,  words?: string[]): string {
+function generateWords(number: number): string {
   let remainder: number, word: string,
     words: string[] = arguments[1];
 
